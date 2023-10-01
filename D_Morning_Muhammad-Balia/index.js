e722cbc1e8b9f010e02ad5f5b7645689d8bd651d
MuhammadBalia <140577711+MuhammadBalia@users.noreply.github.com>
2023-10-01 21:35:29 +0700

    Complete

---
+++ D_Morning_Muhammad-Balia/index.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function () {
+  var preloader = document.querySelector('.preloader');
+  setTimeout(function () {
+    preloader.style.display = 'none';
+  }, 400);
+});
